0c3d6ffc73f5de5b8033c8f9671cf8c4ae09e13e
RaidenFia <70146560+RaidenFia@users.noreply.github.com>
2021-03-03 12:08:30 -0500

    Lab 1

---
+++ Lab 1.py
@@ -0,0 +1,65 @@
+#strings
+name = "Raiden Fia"
+greeting = "Hola"
+age_as_string = "19"
+
+#Integers
+money = 200
+age = 19
+
+#Booleans
+is_male = True
+
+#float
+height = 5.11
+
+    
+input1 = 10
+input2 = 5
+
+#addition
+print(input1 + input2)
+
+#Subtraction
+print(input1 - input2)
+
+#Division
+print(input1 / input2)
+
+#Multiplication
+print(input1 * input2)
+
+#Modulo
+print(input1 % input2)
+
+#Two variables and addition
+
+input3 = 56
+input4 = 1
+
+print(input3 + input4)
+
+#variable into string
+money = "37"
+
+print("The amount of money i have is",(money))
+
+#variable into an integer
+ticket = 10
+print(ticket)
+
+#variable into float
+price = 299.99
+print(price)
+
+#variable into boolean
+fine = 37
+ticket = True
+
+if fine > 36:
+    print("you have to pay for the ticket")
+
+#lists
+
+names = ["Bob", "Jane", "Olivia", "Jesse", "James"]
+print(names)
